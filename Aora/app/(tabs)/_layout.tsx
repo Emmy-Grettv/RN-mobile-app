8864e568a85944ef48ee77f17ef56c0c83629cb8
Emmy Gretta <karabogretta@gmail.com>
2024-10-10 20:34:58 +0200

    Setting home andlayout

--- Aora/app/(tabs)/_layout.tsx
+++ Aora/app/(tabs)/_layout.tsx
@@ -1,37 +1,12 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Slot, Stack } from "expo-router"
+import { Text } from "react-native"
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const RootLayout = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+   <Stack>
+     <Stack.Screen name="/(tabs)/profile" options={{ headerShown: false }} />
+   </Stack>
+  )
 }
+
+export default RootLayout;
